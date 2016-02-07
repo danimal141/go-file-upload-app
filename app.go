13f035995d3f52607e8edbe9851190ab547d7c40
danimal141 <hideaki.ishii1204@gmail.com>
2016-02-07 16:33:07 +0900

    Implement to upload images

---
+++ app.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"image"
+	"image/jpeg"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+var templates = template.Must(template.ParseFiles("templates/index.html", "templates/show.html"))
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{"Title": "index"}
+	renderTemplate(w, "index", data)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Allowed POST method only", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseMultipartForm(32 << 20) // maxMemory
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	form := r.MultipartForm
+	file, err := form.File["upload"][0].Open() // not deal with multiple file upload
+	defer file.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	dest, err := os.Create("/tmp/test.jpg")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if _, err := io.Copy(dest, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/show", http.StatusFound)
+}
+
+func ShowHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("/tmp/test.jpg")
+	defer file.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writeImage(w, "show", &img)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	if err := templates.ExecuteTemplate(w, tmpl+".html", data); err != nil {
+		log.Fatalln("Unable to execute template.")
+	}
+}
+
+func writeImage(w http.ResponseWriter, tmpl string, img *image.Image) {
+	buffer := new(bytes.Buffer)
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		log.Fatalln("Unable to encode image.")
+	}
+	//	w.Header().Set("Content-Type", "image/jpeg")
+	//	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+	//	if _, err := w.Write(buffer.Bytes()); err != nil {
+	//		log.Println("unable to write image.")
+	//	}
+	str := base64.StdEncoding.EncodeToString(buffer.Bytes())
+	data := map[string]interface{}{"Title": tmpl, "Image": str}
+	renderTemplate(w, tmpl, data)
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/upload", UploadHandler)
+	http.HandleFunc("/show", ShowHandler)
+	http.ListenAndServe(":8888", nil)
+}
